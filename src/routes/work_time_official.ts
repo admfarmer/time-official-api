400c45752660e615d3b6ba4b7ba66d284e00b7d2
thawatchai <thawatchai.sea2@gmail.com>
2020-12-08 16:05:08 +0700

    person

--- src/routes/work_time_official.ts
+++ src/routes/work_time_official.ts
@@ -212,6 +212,16 @@ const router = (fastify, { }, next) => {
     }
   })
 
+  fastify.post('/personCid', { preHandler: [fastify.authenticate] }, async (req: fastify.Request, reply: fastify.Reply) => {
+    const cid: any = req.body.cid
+    try {
+      const rs: any = await workTimeOfficialModel.person_cid(db, cid);
+      reply.status(HttpStatus.OK).send({ statusCode: HttpStatus.OK, info: rs, });
+    } catch (error) {
+      fastify.log.error(error);
+      reply.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) })
+    }
+  })
   next();
 }
 
